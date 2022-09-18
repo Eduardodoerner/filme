0d4c5c6a62fe609d41d4354f6d9c3b1e90874b40
Eduardodoerner <110205124+Eduardodoerner@users.noreply.github.com>
2022-09-18 10:18:43 -0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,5 @@
+<a class="btn btn-link" href="?">home</a>
+<br>
+<a class="btn btn-link" href="?pagina=filmes_listagem">filmes</a>
+<br>
+<a class="btn btn-link" href="?pagina=contato&nome=avatar">contato</a>
